29b4d6c4951d25321f9fc38668e27981fbdbb77f
marcinKgit1 <marcinkozlowski390@gmail.com>
2026-03-13 17:48:56 +0100

    refactor: standardize code formatting and improve readability in multiple files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import { AppRoutes } from './routes';
-import { useCurrencyStore } from '../stores/useCurrencyStore';
-import '../i18n';
+import React, { useEffect } from "react";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+import { useCurrencyStore } from "../stores/useCurrencyStore";
+import "../i18n";
 
 export function App() {
   const fetchRates = useCurrencyStore((state) => state.fetchRates);
-  const Router = typeof window !== 'undefined' && window.location.hostname.endsWith('github.io')
-    ? HashRouter
-    : BrowserRouter;
+  const Router =
+    typeof window !== "undefined" &&
+    window.location.hostname.endsWith("github.io")
+      ? HashRouter
+      : BrowserRouter;
 
   useEffect(() => {
     fetchRates();

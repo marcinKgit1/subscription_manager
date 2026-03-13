fcebba67bccf59cff07fd7421a4cc7a43ac5cc00
marcinKgit1 <marcinkozlowski390@gmail.com>
2026-03-13 17:48:41 +0100

    feat: update title in index.html, refactor routing in App component, and add GitHub Pages deployment workflow

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { useCurrencyStore } from '../stores/useCurrencyStore';
 import '../i18n';
 
 export function App() {
   const fetchRates = useCurrencyStore((state) => state.fetchRates);
+  const Router = typeof window !== 'undefined' && window.location.hostname.endsWith('github.io')
+    ? HashRouter
+    : BrowserRouter;
 
   useEffect(() => {
     fetchRates();
   }, [fetchRates]);
 
   return (
-    <BrowserRouter>
+    <Router>
       <AppRoutes />
-    </BrowserRouter>
+    </Router>
   );
 }
